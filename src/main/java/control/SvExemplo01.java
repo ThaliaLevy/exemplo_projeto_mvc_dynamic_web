5bb74e03fef83ee34687c0f7b63f99bcf4f771b0
ThaliaLevy <thalialevy@hotmail.com>
2022-04-12 16:12:54 -0300

    '.'

---
+++ src/main/java/control/SvExemplo01.java
@@ -0,0 +1,23 @@
+package control;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//localização do meu servlet > não se deve alterar!
+@WebServlet("/SvExemplo01")
+
+public class SvExemplo01 extends HttpServlet {	
+	//request e response são variáveis
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//no request coloca-se
+}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
