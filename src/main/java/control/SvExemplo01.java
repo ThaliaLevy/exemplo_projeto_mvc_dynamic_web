1164b368ac029dc3856b0ff16223b567ac4172a7
ThaliaLevy <thalialevy@hotmail.com>
2022-04-15 10:06:14 -0300

    finalizando primeiro exemplo do uso do doGet

--- src/main/java/control/SvExemplo01.java
+++ src/main/java/control/SvExemplo01.java
@@ -13,7 +13,13 @@ import javax.servlet.http.HttpServletResponse;
 public class SvExemplo01 extends HttpServlet {	
 	//request e response são variáveis
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nomeRecebido = request.getParameter("nomeCadastrado");
+		int nota1 = Integer.parseInt(request.getParameter("nota1"));
+		int nota2 = Integer.parseInt(request.getParameter("nota2"));
+		int nota3 = Integer.parseInt(request.getParameter("nota3"));
+		int nota4 = Integer.parseInt(request.getParameter("nota4"));
 		
+		double media = (nota1 + nota2 + nota3 + nota4)/4;
 }
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
