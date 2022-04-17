786fb5fb597a431055c3956c2c0e7dfc8cb81eec
ThaliaLevy <thalialevy@hotmail.com>
2022-04-17 18:03:07 -0300

    .

--- src/main/java/control/SvCarroCadastrar.java
+++ src/main/java/control/SvCarroCadastrar.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 //localização do meu servlet > não se deve alterar!
 @WebServlet("/SvExemplo01")
 
-public class SvExemplo01 extends HttpServlet {	
+public class SvCarroCadastrar extends HttpServlet {	
 	//request e response são variáveis
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//pegar informações que serão usadas, vindas do arquivo .html de entrada 
